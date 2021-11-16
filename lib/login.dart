ca70a8c323c7cca4ab8da014be02336f8293818d
DELL <dalijlasii@gmail.com>
2021-11-17 00:06:04 +0100

    try another theme

--- lib/login.dart
+++ lib/login.dart
@@ -52,7 +52,7 @@ class _LoginPageState extends State<LoginPage> {
             const SizedBox(height: 80.0),
             Column(
               children: <Widget>[
-                Image.asset('assets/diamond.png'),
+                Image.asset('assets/diamond.png', color: kShrineBlack),
                 const SizedBox(height: 16.0),
                 Text(
                   'SHRINE',
